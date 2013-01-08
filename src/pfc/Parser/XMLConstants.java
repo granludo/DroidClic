5610da0f84dab717a5f72421c55ed11400cf525b
maria-claver <maria.claver@gmail.com>
2013-01-08 16:36:14 +0100

    added LICENSE

--- src/pfc/Parser/XMLConstants.java
+++ src/pfc/Parser/XMLConstants.java
@@ -1,8 +1,152 @@
 /*
- * @Projecte: JClic per gPhone
- * @Autora: Miriam Pujol Benet
- * @Versio: Juny 2009
- */
+* This file is part of DroidClic
+*
+* DroidClic is copyright 2012 by
+* 	Marc Alier Forment,
+* 	Maria José Casany Guerrero,
+* 	Enric Mayol
+* 
+* UPC Students involved in this project:
+* 
+* Previous version and legacy code:
+* ---------------------------------
+* 	PUJOL BENET, MIRIAM
+*
+*
+* Project management
+* ------------------
+*	ALMA SERRANO, ALBERT
+* 	CLAVER ARGUDO, MARIA
+*	JIMENEZ TARRES, VICTOR
+*	CORCHADO MERINO, JUAN CARLOS
+* 	JUAN JANE, ANDREU
+*	MENES ROUCO, MARTIN
+*	ORTEGA GOMEZ, CRISTIAN
+*	PURCET SOTO, SERGI
+*	RAMOS GONZALEZ, RICARDO
+* 	SOLE MORERA, DANIEL
+*
+*
+* Research & support
+* --------------------
+*	ALBALATE FERNANDEZ, AIDA
+* 	CABRE JUAN, ALBERT
+* 	CANDON ARENAS, HECTOR
+*	ELBAILE SERRA, ABEL
+* 	GONZALEZ DE PABLO, BORJA
+*	IGLESIAS LOPEZ, OSCAR
+* 	MARTINEZ LOPEZ, SERGIO
+*	PEREZ PLANAS, ORIAC
+*	SANCHEZ MARCOS, IVAN
+* 	TORNE GOZALBO, ORIOL
+*
+*
+* Development
+* -----------
+* Lead developers
+*	ALBALATE FERNANDEZ, AIDA
+*	COSTA MANSILLA, GERARD
+* 	GONZALEZ DE PABLO, BORJA
+* Developers:
+* 	ALEMANY FONT, ALBERT
+* 	ALVAREZ JUSTE, XAVIER
+* 	ALVAREZ MORALES, FERRAN
+* 	BARRERO MARTINEZ, LINDSAY
+* 	BENITEZ VALLS, ALBERT
+* 	BERRUEZO MARTINEZ, DAVID
+*	BRAMON DEVANT, MARC
+* 	BRIGUELLI DA SILVA, LUIS FERNANDO
+* 	CABRE JUAN, ALBERT
+* 	CANDON ARENAS, HECTOR
+* 	CAPEL CATALAN, VICTOR
+*	CLAVER ARGUDO, MARIA
+*	DE PAULA DE PUIG GUIXE, FRANCESC
+* 	DIEZ RUIZ, ALBERT
+*	ELBAILE SERRA, ABEL
+* 	FARRE GONZALEZ, PAU
+*	GARCIA GARCIA, XAVIER
+* 	HURTADO OBIOLS, CRISTINA
+* 	MARTINEZ DIAZ, ARTURO
+* 	MARTINEZ LOPEZ, SERGIO
+*	MENES ROUCO, MARTIN
+* 	MONTSERRAT GARCIA, EDUARD
+* 	ORTIZ GRIMAU, XAVIER
+* 	OSORIO ALVAREZ, DAVID
+*	PASCUAL VAZQUEZ, PABLO
+* 	PEDRAZA GUTIERREZ, M. MERCEDES
+* 	PEREZ PLANAS, ORIAC
+* 	RODRIGUEZ TORRES, MIREIA
+* 	SANCHEZ MARCOS, IVAN
+*	SEGARRA RODA, EDUARD
+*	SELLES FEITO, MANEL
+*	SOLER PASCUAL, GERARD
+*	SUBIRATS SALVANS, JOAN
+*
+*
+* Design & usability
+* --------------------
+* Lead designer:
+* 	LEGORBURU CLADERA, IÑIGO
+* Designers:
+* 	OTAL RODRIGUEZ, DANIEL
+*	PASCUAL VAZQUEZ, PABLO
+*	SEGARRA RODA, EDUARD
+*	SOLER PASCUAL, GERARD
+*	SUBIRATS SALVANS, JOAN
+* 	VIDAL PASTALLE, MARIA
+*
+*
+* Testing, evaluation & audit
+* ---------------------------
+* Lead tester:
+* 	NAVARRO JIMENEZ, GERMAN
+*	ALEMANY FONT, ALBERT
+* Testers:
+*	ALVAREZ MORALES, FERRAN
+*	BENITEZ VALLS, ALBERT
+* 	CAPEL CATALAN, VICTOR
+*	MONTSERRAT GARCIA, EDUARD
+* 	ORTIZ GRIMAU, XAVIER
+* 	SANCHEZ CORREDOR, MONTSERRAT
+*
+*
+* Documentation, communication & broadcast
+* ----------------------------------------
+* Lead documentator:
+*	ALVAREZ JUSTE, XAVIER
+*	SANCHEZ CORREDOR, MONTSERRAT
+* Documentators:
+*	BARRERO MARTINEZ, LINDSAY
+* 	GARCIA GARCIA, XAVIER
+*	NAVARRO JIMENEZ, GERMAN
+*	OSORIO ALVAREZ, DAVID
+*	TORNE GOZALBO, ORIOL
+*
+* 
+* DroidClic is copyright 2012 by
+* Universitat Politecnica de Catalunya http://www.upc.edu
+* Contact info:
+* Marc Alier Forment granludo @ gmail.com or marc.alier @ upc.edu 
+*
+* DroiClic is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Droidlic is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DroidClic. If not, see <http://www.gnu.org/licenses/>.
+*
+* DroidClic is based on the Software JClic by Francesc Busquets
+* http://clic.xtec.cat/es/jclic/ 
+*
+*/
+
+
 
 package pfc.Parser;
 
